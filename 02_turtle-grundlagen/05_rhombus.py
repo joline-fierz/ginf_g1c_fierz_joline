cf337d2222df6f2c10a493a8b260d24f4328830c
Joline <joline.fierz@stud.kswe.ch>
2025-08-22 09:22:54 +0200

    Kapitel 2, Übung 11

---
+++ 02_turtle-grundlagen/05_rhombus.py
@@ -0,0 +1,13 @@
+
+import turtle 
+
+turtle.shape("turtle")
+turtle.left(90)
+turtle.left(60)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
