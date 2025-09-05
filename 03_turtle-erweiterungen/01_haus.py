1de6ca30b87093f66347780f8c78519a5637e54c
Joline <joline.fierz@stud.kswe.ch>
2025-09-05 09:09:42 +0200

    Haus

---
+++ 03_turtle-erweiterungen/01_haus.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.lt(90)
+turtle.pensize(3)
+turtle.pencolor("brown")
+turtle.fd(150)
+turtle.rt(90)
+turtle.fd(150)
+turtle.rt(90)
+turtle.fd(150)
+turtle.rt(90)
+turtle.fd(150)
+turtle.rt(90)
+turtle.fd(150)
+turtle.pencolor("red")
+turtle.rt(30)
+turtle.fd(150)
+turtle.rt(120)
+turtle.fd(150)
+
+
+
