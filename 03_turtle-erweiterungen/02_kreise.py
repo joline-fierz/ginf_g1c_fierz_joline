ecd4cc91bd31040a32c434b1ecb43a61160166e0
Joline <joline.fierz@stud.kswe.ch>
2025-09-13 22:31:48 +0200

    Kreise

---
+++ 03_turtle-erweiterungen/02_kreise.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.pencolor("red")
+turtle.speed(1)
+turtle.circle(25)
+turtle.rt(90)
+turtle.pencolor("green")
+turtle.speed(3)
+turtle.circle(50)
+turtle.rt(90)
+turtle.pencolor("pink")
+turtle.speed(6)
+turtle.circle(75)
+turtle.rt(90)
+turtle.pencolor("yellow")
+turtle.speed(10)
+turtle.circle(100)
+
+
