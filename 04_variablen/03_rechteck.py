6d3b2d69db6c0d64d15ca6676f1d8dac1d793bff
Joline <joline.fierz@stud.kswe.ch>
2025-09-14 22:15:10 +0200

    Rechteck mit drei Variabeln

---
+++ 04_variablen/03_rechteck.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.shape("turtle")
+a = 200
+b = 100
+c = 90
+turtle.pencolor("navy")
+turtle.fd(a)
+turtle.lt(c)
+turtle.pencolor("tan")
+turtle.fd(b)
+turtle.lt(c)
+turtle.pencolor("navy")
+turtle.fd(a)
+turtle.lt(c)
+turtle.pencolor("tan")
+turtle.fd(b)
+
+
+
