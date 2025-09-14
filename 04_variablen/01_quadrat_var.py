1cfaf709173fc430cc4027ddcaa8a21c9ea58df6
Joline <joline.fierz@stud.kswe.ch>
2025-09-14 16:26:50 +0200

    Quadrat mit Variable

---
+++ 04_variablen/01_quadrat_var.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.pencolor("purple")
+a = 100
+b = 90
+turtle.fd(a)
+turtle.lt(b)
+turtle.fd(a)
+turtle.lt(b)
+turtle.fd(a)
+turtle.lt(b)
+turtle.fd(a)
+
