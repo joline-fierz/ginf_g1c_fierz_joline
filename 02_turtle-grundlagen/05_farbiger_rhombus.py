19e51a6727af93818a322b6eec1d65bab69b0666
Joline <joline.fierz@stud.kswe.ch>
2025-10-19 11:05:26 +0200

    Farbiger Rhombus

---
+++ 02_turtle-grundlagen/05_farbiger_rhombus.py
@@ -0,0 +1,18 @@
+import turtle
+
+s = 100
+t = "turquoise"
+size = 3
+a = 60
+b = 120
+turtle.shape("turtle")
+turtle.pensize(size)
+turtle.pencolor(t)
+turtle.lt(30)
+turtle.fd(s)
+turtle.lt(b)
+turtle.fd(s)
+turtle.lt(a)
+turtle.fd(s)
+turtle.lt(b)
+turtle.fd(s)
