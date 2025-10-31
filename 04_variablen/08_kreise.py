38f8708ee3ddd6f57ac6e9b19f102725b45289b0
Joline <joline.fierz@stud.kswe.ch>
2025-10-31 12:42:43 +0100

    Kreise

---
+++ 04_variablen/08_kreise.py
@@ -0,0 +1,27 @@
+import turtle
+import random
+turtle.shape("turtle")
+farbe_1 = "red"
+farbe_2 = "green"
+farbe_3 = "blue"
+farbe_4 = "pink"
+radius = random.randrange(50,76)
+turtle.pencolor(farbe_1)
+turtle.circle(radius)
+turtle.pu()
+turtle.fd(radius)
+turtle.pencolor(farbe_2)
+turtle.pd()
+turtle.circle(radius)
+turtle.pu()
+turtle.fd(radius)
+turtle.pencolor(farbe_3)
+turtle.pd()
+turtle.circle(radius)
+turtle.pu()
+turtle.fd(radius)
+turtle.pencolor(farbe_4)
+turtle.pd()
+turtle.circle(radius)
+turtle.pu()
+turtle.fd(radius)
