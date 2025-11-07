8b13355de49ee22f5749e692f6ab4ede04c644f2
Joline <joline.fierz@stud.kswe.ch>
2025-11-07 08:48:54 +0100

    Zufallsdreieck

---
+++ 04_variablen/10_zufallsdreieck.py
@@ -0,0 +1,27 @@
+import random
+import turtle 
+
+d = random.randrange(10, 26)
+starke = random.randrange(1, 6)
+x1 = random.randrange(-100, 100)
+x2 = random.randrange(-100, 100)
+x3 = random.randrange(-100, 100)
+y1 = random.randrange(-200, 175)
+y2 = random.randrange(-200, 175)
+y3 = random.randrange(-200, 175)
+farbe_1 = "red"
+farbe_2 = "green"
+farbe_3 = "blue"
+turtle.pensize(starke)
+turtle.pu()
+turtle.goto(x1, y1)
+turtle.pd()
+turtle.pencolor(farbe_1)
+turtle.dot(d)
+turtle.goto(x2, y2)
+turtle.pencolor(farbe_2)
+turtle.dot(d)
+turtle.goto(x3, y3)
+turtle.pencolor(farbe_3)
+turtle.dot(d)
+turtle.goto(x1, y1)
