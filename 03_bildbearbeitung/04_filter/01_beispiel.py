000869c681342f8672ce8bddbdb51208ccdd8d98
Joline <joline.fierz@stud.kswe.ch>
2026-06-11 15:44:48 +0200

    Übung 1, Kapitel 9

---
+++ 03_bildbearbeitung/04_filter/01_beispiel.py
@@ -0,0 +1,18 @@
+import PIL.Image as img
+
+bild = img.open("portland.png")
+
+for x in range(0, bild.width):
+    for y in range(0, bild.height):
+        #getpixel: geht zu den Kordinaten (x, y)
+        # Kopiert den Farbwert in die Variabeln r, g und b
+        r, g, b = bild.getpixel((x, y))
+
+        #Verdoppelt den Farbwert
+        #Bild wird neu doppelt so hell.
+        r_neu = 2 * r
+        g_neu = 2 * g
+        b_neu = 2 * b
+        
+        bild.putpixel((x, y), (r_neu, g_neu, b_neu))
+bild.save("01_beispiel_ergebnis.png")
